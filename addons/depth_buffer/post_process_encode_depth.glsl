46eb499fa5331ce7a53e3bacb1467f4f45dcc91a
Josh Carlson <22630098+paperman5@users.noreply.github.com>
2024-09-12 22:14:42 -0700

    Memory restrictions on color_image in compute shader

--- addons/depth_buffer/post_process_encode_depth.glsl
+++ addons/depth_buffer/post_process_encode_depth.glsl
@@ -4,7 +4,7 @@
 // Invocations in the (x, y, z) dimension
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
-layout(rgba16f, set = 0, binding = 0) uniform image2D color_image;
+layout(rgba16f, set = 0, binding = 0) uniform restrict writeonly image2D color_image;
 layout(set = 0, binding = 1) uniform sampler2D depth_image;
 
 // Our push constant
